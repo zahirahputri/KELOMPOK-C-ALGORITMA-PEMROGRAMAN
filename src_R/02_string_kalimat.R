ce3a49e37b2a785764f6f9b0304803cc66632e89
sriutamids <sri.utami.d@mhs.unsoed.ac.id>
2026-06-08 23:57:08 +0700

    Add files via upload

---
+++ src_R/02_string_kalimat.R
@@ -0,0 +1,9 @@
+K1 <- "Saya tak 'kan menyerah."
+K2 <- 'Ia berkata, "Aku menyayangimu."'
+K3 <- "\"Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!\""
+K4 <- "Surat keputusan itu bernomor 62/UN.34/19/2023."
+
+cat(K1, "\n")
+cat(K2, "\n")
+cat(K3, "\n")
+cat(K4, "\n")
