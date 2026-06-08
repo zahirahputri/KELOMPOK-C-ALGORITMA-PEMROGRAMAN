905bfe83c4a442a54ec63abb92da9c8c32497c54
gabrielputri-commits <gabriel.putri@mhs.unsoed.ac.id>
2026-06-08 23:43:40 +0700

    01_analisis_teks.R

--- src_R/01_analisis_teks.R
+++ src_R/01_analisis_teks.R
@@ -1,1 +1,12 @@
-
+hitung_teks<-function(teks){
+  kata<- unlist(strsplit(teks,"\\s+"))
+  jumlah_kata<-length(kata)
+  
+  kalimat<-unlist(strsplit(teks,"\\."))
+  kalimat<-kalimat[kalimat != ""]
+  
+  jumlah_kalimat<-length(kalimat)
+  cat("Teks tersebut memuat",jumlah_kalimat,"kalimat dan",jumlah_kata,"kata.\n")
+}
+teks_input<-"Media sosial atau disebut juga dengan jejaring sosial, seperti Facebook, Twitter, Instagram, dan masih banyak lagi ternyata tidak hanya digunakan sebagai tempat berkumpul atau berbagi di dunia maya. Namun, media sosial kini juga bisa dimanfaatkan sebagai media untuk mengembangkan sebuah bisnis. Saat ini telah banyak para pengusaha yang beralih ke media sosial dalam memasarkan produk mereka baik barang ataupun jasa. Beralihnya para pelaku bisnis ke media ini dikarenakan jejaring sosial memiliki manfaat yang sangat banyak bagi usaha bisnis. Berikut ini adalah alasan mengapa jejaring sosial bisa menjadi alat promosi yang paling efektif."
+hitung_teks(teks_input)
