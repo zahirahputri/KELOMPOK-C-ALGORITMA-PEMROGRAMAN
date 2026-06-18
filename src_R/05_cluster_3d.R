0d0f1f0d20629304ab37ce50b4f5cdffdb5371cb
lilie-ui <lilyanamayhandayanii@gmail.com>
2026-06-18 10:34:50 +0700

    Update 05_cluster_3d.R

--- src_R/05_cluster_3d.R
+++ src_R/05_cluster_3d.R
@@ -3,14 +3,12 @@ pusat_A <- c(2, 1, 3)
 pusat_B <- c(1, -4, 6)
 pusat_C <- c(-2, 3, -2)
 
-
 #Hitung jarak Euclidean 
 hitung_jarak <- function(titik_u, titik_pusat) {
   selisih <- titik_u - titik_pusat
   jarak   <- sqrt(sum(selisih^2))
   return(jarak)
 }
-
 
 #Tentukan cluster terdekat 
 tentukan_cluster <- function(titik_u) {
@@ -34,7 +32,6 @@ tentukan_cluster <- function(titik_u) {
               cluster = cluster))
 }
 
-
 #FUNGSI 3: Cetak hasil 
 cetak_hasil <- function(nomor, titik_u, hasil) {
   cat("Kondisi", nomor, ": U =", titik_u, "\n")
@@ -44,7 +41,6 @@ cetak_hasil <- function(nomor, titik_u, hasil) {
   cat("=> Titik U tergolong Cluster", hasil$cluster, "\n")
 }
 
-
 #MAIN PROGRAM 
 cat("PROGRAM 5 - KLASIFIKASI CLUSTER 3D\n\n")
 
@@ -53,12 +49,10 @@ u1     <- c(3, 1, 4)
 hasil1 <- tentukan_cluster(u1)
 cetak_hasil(1, u1, hasil1)
 
-
 # Kondisi 2: dekat Cluster B
 u2     <- c(2, -5, 7)
 hasil2 <- tentukan_cluster(u2)
 cetak_hasil(2, u2, hasil2)
-
 
 # Kondisi 3: dekat Cluster C
 u3     <- c(-3, 4, -2)
